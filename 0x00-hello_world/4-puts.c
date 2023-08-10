0c6d39a306cf51c5b8499a56a5939b45780bf333
Super User <rahmakh248@gmail.com>
2023-08-10 11:21:33 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
+/*
+ * file : 4-puts.c
+ * code written by : Rahma Khaled
+ * email: rahmakh248@gmail.com
+ */
 #include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual puzzle, followed by a new line.
+ * Return: Always 0.
+ */
 int main(void)
-/* main function */	
 {
-      puts("\"Programming is like building a multilingual puzzle");
-      return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
