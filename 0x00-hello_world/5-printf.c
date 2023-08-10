01cc1da62a65fae591704f7f6fe5229101cbfe5b
Super User <rahmakh248@gmail.com>
2023-08-10 11:33:40 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ * file name :5-printf.c
+ * code written by : Rahma Khaled
+ */
+int main(void)
+{	
+/*
+ * main function prints  with proper grammar,
+ * but the outcome is a piece of art,
+ * , followed by a new line.
+ */
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
