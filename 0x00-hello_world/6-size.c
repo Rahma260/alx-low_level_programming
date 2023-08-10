3beda7effaa89150e5dc0bdc53486b1cd48018d5
Super User <rahmakh248@gmail.com>
2023-08-10 11:57:24 -0700

    done

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+/*
+ * code written by Rahma Khaled
+ */
+#include <stdio.h>
+/**
+ * main - Prints the size of various types on
+ *               the computer it is compiled and run on
+ *
+ * Return: Always 0.
+ */
+int main (void)
+{
+	printf(sizeof(char));
+	printf(sizeof(int));
+	printf(sizeof(long int));
+	printf(sizeof(long long int));
+	printf(sizeof(float));
+	return (0);
