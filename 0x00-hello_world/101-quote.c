1363fc8ab0068503e011f492453a876c0315ec7d
Super User <rahmakh248@gmail.com>
2023-08-10 13:02:23 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/*
+ * file : 4-puts.c
+ * code written by : Rahma Khaled
+ * email: rahmakh248@gmail.com
+ */
+#include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual
+ *puzzle, followed by a new line.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
