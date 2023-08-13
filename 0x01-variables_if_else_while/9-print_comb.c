2b9723cefa0b2a668f6ef9e12d38d6fb2f94aff4
Super User <rahmakh248@gmail.com>
2023-08-13 13:28:41 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+/*
+ * file : 9-print_comb.c
+ * code written by : Rahma Khaled
+ */
+#include <stdio.h>
+
+/**
+ * main - Prints all possible combinations of single-digit numbers.
+ * , followed by a new line and comma.
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + 48);
+		if (i != 9)
+		{
+			putchar(',');
+		}
+		putchar(' ');
+	}
+
+	
+
+	putchar('\n');
+
+	return (0);
+}
