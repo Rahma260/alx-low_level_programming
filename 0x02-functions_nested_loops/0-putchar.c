ddf8fbee6581c20d98da32615dd506e6de3172ba
Super User <rahmakh248@gmail.com>
2023-08-15 10:30:29 -0700

    done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,13 +2,20 @@
  * file : 0-putchar.c
  * code written by : Rahma Khaled
  */
-#include <stdio.h>
+#include <main.h>
 /**
  * main - Prints _putchar, followed by a new line.
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
