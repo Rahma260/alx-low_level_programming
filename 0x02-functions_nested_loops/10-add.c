8abc35b485f0be93723d9bd21a550122d3643311
Super User <rahmakh248@gmail.com>
2023-08-15 14:45:59 -0700

    done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+/*
+ * file :10-add.c
+ * code written by : Rahma Khaled
+ */
+#include "main.h"
+/**
+  * add  - Add two numbers
+  * @i: The first number to be computed
+  * @j: The second number to be computed
+  * Return: the sum of two numbers
+  */
+int add(int i, int j)
+{
+	int sum = i + j;
+
+	return (sum);
+
+	_putchar('\n');
+}
