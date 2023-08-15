216cfb6ed275eec4fa6953e02ac3587ef766d25f
Super User <rahmakh248@gmail.com>
2023-08-15 11:37:47 -0700

    done

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 /*
- * file : 1-alphabet.c
+ * file :2-print_alphabet_x10.c
  * code written by : Rahma Khaled
  */
 #include "main.h"
 /**
-  * print_alphabet - Make the alphabet
+  * print_alphabet_x10 - Make the alphabet
   *
   * Return: void
   */
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 	char a;
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 		{
