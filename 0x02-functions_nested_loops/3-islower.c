d8b45669a4d12f1e184e803e24ade406b34469b0
Super User <rahmakh248@gmail.com>
2023-08-15 12:12:21 -0700

    done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+/*
+ * file : 3-islower.c
+ * code written by : Rahma Khaled
+ */
+#include "main.h"
+/**
+  * _islower  -  Checks for lowercase character.
+  *
+  * Return:1 if c is lowercase
+  * 0 otherwise
+  */
+int _islower(int i)
+{
+
+	if (i >= 97 && i <= 122)
+		return (1);
+	else
+		return (0);
+
+	_putchar('\n');
+
+}
