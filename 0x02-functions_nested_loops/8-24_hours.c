fd7a87c783f8c562eaf6b56337cc709778a50775
Super User <rahmakh248@gmail.com>
2023-08-16 10:49:06 -0700

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,18 +10,21 @@
   */
 void jack_bauer(void)
 {
-	int a;
+	int a, b, c, d;
 
 	for (a = 0; a <= 2; a++)
 	{
-		for (a = 0; a <= 3; a++)
+		for (b = 0; b <= 3; b++)
 		{
-			_putchar(':');
-			for (a = 0; a <= 5; a++)
+			for (c = 0; c <= 5; c++)
 			{
-				for (a = 0; a <= 9; a++)
+				for (d = 0; d <= 9; d++)
 				{
 					_putchar(a + 48);
+					_putchar(b + 48);
+					_putchar(58);
+					_putchar(c + 48);
+					_putchar(d + 48);
 					_putchar('\n');
 				}
 			}
