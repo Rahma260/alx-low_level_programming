6c4507045de6b217eb6a450b873ad52ac4d6c6eb
Super User <rahmakh248@gmail.com>
2023-08-17 08:26:08 -0700

    done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+/*
+ * file : 4-print_most_numbers.c
+ * code written by : Rahma Khaled
+ */
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints the numbers, from 0 to 9
+ * without 2 and 4, followed by a new line
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (((i + 48) == 2) || ((i + 48) == 4))
+		_putchar(i + 48);
+	}
+
+	_putchar('\n');
+
+}
