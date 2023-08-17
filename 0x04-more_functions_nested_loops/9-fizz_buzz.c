e626a400eff978285b439601ef153e08742b20be
Super User <rahmakh248@gmail.com>
2023-08-17 12:19:30 -0700

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,19 +21,21 @@ int main(void)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if ((i % 3) == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		putchar(' ');
 	}
-	putchar('\n');
+	if (i != 100)
+		putchar('\n');
 	return (0);
 }
