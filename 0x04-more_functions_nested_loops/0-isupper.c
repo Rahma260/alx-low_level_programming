71f5c8d33058ac90ade5c58cf93679c7fe1a2b85
Super User <rahmakh248@gmail.com>
2023-08-17 07:34:45 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+/*
+ * file : 0-isupper.c
+ * code written by : Rahma Khaled
+ */
+#include "main.h"
+/**
+  * _isupper - Checks for uppercase character.
+  * @i: The character to be checked
+  * Return: 1 if c is uppercase ,0 otherwise
+  */
+int _isupper(int i)
+{
+
+	if (i >= 65 || i <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
