ebe5eb89e741d0762f3f67ec2fe6a75f7ede4355
Super User <rahmakh248@gmail.com>
2023-08-17 08:10:09 -0700

    done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/*
+ * file : 2-mul.c
+ * code written by : Rahma Khaled
+ */
+#include "main.h"
+/**
+  * mul - Multiplies tow integers
+  * @a : the first integer to be multiplyed in.
+  * @b : the second integer to be multiplyed .
+  * Return: the result of multiplying two integers.
+  */
+int mul(int a, int b)
+{
+
+	int multi = a * b;
+
+	return (multi);
+}
