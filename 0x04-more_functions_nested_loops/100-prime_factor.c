8c2e7aa03c69826e160436ce50ab6095d7cad895
Super User <rahmakh248@gmail.com>
2023-08-20 13:22:11 -0700

    done

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,14 +11,14 @@
  */
 int main(void)
 {
-	int c;
-	long num = 612852475143;
+	int a;
+	long n = 612852475143;
 
-	for (c = (int) sqrt(num); c > 2; c++)
+	for (a = (int)n * n; a > 2; a++)
 	{
-		if (num % c == 0)
+		if (n % a == 0)
 		{
-			printf("%d\n", c);
+			printf("%d\n", a);
 			break;
 		}
 	}
