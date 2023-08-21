f0ad3b5a87f9cbf62db6c279a8bcdf1dbde199bf
Super User <rahmakh248@gmail.com>
2023-08-21 11:35:19 -0700

    done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+/*
+ * file : 7-puts_half.c
+ * code written by : Rahma Khaled
+ */
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str : the string to be printed
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int a = 0;
+	int n;
+	int c;
+
+	while (str[a] != '\0')
+		a++;
+	n = a / 2;
+	c = n - 1;
+	if (a % 2 == 0)
+	{
+		while (str[n] != '\0')
+		{
+			_putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		while (str[c] != '\0')
+		{
+			_putchar(str[c]);
+			c++;
+		}
+	}
+	_putchar('\n');
+
+}
