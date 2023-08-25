8586a275ff8b057ae4761a3a2ee9da9b540c1052
Super User <rahmakh248@gmail.com>
2023-08-25 11:05:30 -0700

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,19 +13,11 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
 
-	for (; s1[i] != '\0'; i++)
+	for (; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		for (; s2[j] != '\0'; j++)
-		{
-			if (s1[i] > s2[j])
-				return (15);
-			else if (s1[i] < s2[j])
-				return (-15);
-			else
-				return (0);
-		}
+		if (s1[i] > s2[i] || s1[i] < s2[i])
+			return (s1[i] - s2[i]);
 	}
 	return (0);
 }
