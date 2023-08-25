9d80fcb43943e8d4dbb651f50ef064da1da13665
Super User <rahmakh248@gmail.com>
2023-08-25 07:50:05 -0700

    done

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+/*
+ * file : 1-strncat.c
+ * code written by : Rahma Khaled
+ */
+#include "main.h"
+
+/**
+ * _strncat -  concatinates two strings
+ * @src : the string to be concatinate with
+ * @dest : the place to be concatinated
+ * @n : the number of characters will be concatinated
+ * Return: The pointer to dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	for (; dest[i] != '\0'; i++)
+	;
+
+	while (j < n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	return (dest);
+}
