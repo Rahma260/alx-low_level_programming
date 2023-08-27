f5881c6e88267a053b1c85d24719fc730d338a62
Super User <rahmakh248@gmail.com>
2023-08-27 04:33:52 -0700

    done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+/*
+ * file : 4-rev_array.c
+ * code written by : Rahma Khaled
+ */
+#include "main.h"
+
+/**
+ * reverse_array - Reverses an array
+ * @a : the array to be reversed
+ * @n : the number of elements of an array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int strt = 0;
+	int end;
+	char tmp;
+
+	end = n - 1;
+	while (strt < end)
+	{
+		tmp = a[strt];
+		a[strt] = a[end];
+		a[end] = tmp;
+		strt++;
+		end--;
+	}
+}
