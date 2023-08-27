fda941e3b67a2a4e6874879fc1ec902213f2b44b
Super User <rahmakh248@gmail.com>
2023-08-27 14:27:50 -0700

    done

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -3,8 +3,7 @@
  * code written by : Rahma Khaled
  */
 #include "main.h"
-#include <stdio.h>
-#include <limits.h>
+
 /**
  * _atoi - Convert a string to an integer.
  * @s : the string to be converted
@@ -13,25 +12,20 @@
 
 int _atoi(char *s)
 {
-	char sign = 0;
+	int sign = 1;
 	unsigned int num = 0;
 
-	while (!(*s >= '0' && *s <= '9'))
-	{
+	do {
 		if (*s == '-')
-			sign = !sign;
-		s++;
-	}
-	while (*s >= '0' && *s <= '9')
-	{
-		num = num * 10 + (*s - '0');
-		s++;
-		if (!sign && num >= INT_MAX)
-			return (INT_MAX);
-		else if (sign && num > INT_MAX)
-			return (INT_MIN);
-	}
-	if (sign)
-		return (-num);
-	return (num);
+			sign *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * sign);
 }
