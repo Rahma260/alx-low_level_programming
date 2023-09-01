a56a0717c27c97b2b7811380962a8b72cc77103c
Super User <rahmakh248@gmail.com>
2023-09-01 10:45:28 -0700

    done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+/*
+ * file name : 4-add.c
+ * written by : Rshma Khaled
+ */
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - adds two numbers, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char **argv)
+{
+	int i = 1;
+	int j;
+	int add = 0;
+
+	while (i <= argc - 1)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Erorr\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+		i++;
+	}
+	printf("%d\n", add);
+	return (0);
+}
