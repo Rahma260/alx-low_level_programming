3a15e39b9b59121f88ecd08e2a1734fc26f9fd34
Super User <rahmakh248@gmail.com>
2023-09-01 11:01:38 -0700

    done

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,7 +11,7 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 	int i = 1;
 	int j;
