ad20cb1ddcc3b69a7e6ea3ca3281930c567de53f
Super User <rahmakh248@gmail.com>
2023-09-01 09:36:35 -0700

    done

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+/*
+ * file name : 0-whatsmyname.c
+ * written by : Rshma Khaled
+ */
+
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char **argv)
+{
+	int i = 0;
+	while (i <= argc - 1)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
