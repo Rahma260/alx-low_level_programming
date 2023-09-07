18073fe415f456195cafb3e80f607cdbd451435e
Super User <rahmakh248@gmail.com>
2023-09-07 11:31:26 -0700

    done

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+/*
+ * file : 0-malloc_checked
+ * code written by : Rahma Khaled
+ */
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b : the size of memory
+ * Return: void
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *array = malloc(b);
+
+	if (array == NULL)
+		exit(98);
+	return(array);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <stdio.h>
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif
