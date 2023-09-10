b3eb760b8b9e258313061bf4f7d4c254578cbee6
Super User <rahmakh248@gmail.com>
2023-09-10 11:43:02 -0700

    done

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+/*
+ * file : 0-memset.c
+ * code written by : Rahma Khaled
+ */
+#include "main.h"
+
+/**
+ * _memset -  fills memory with a constant byte.
+ * @s : pointer to a memory area
+ * @b : constant byte
+ * @n : bytes of the memory area
+ * Return : pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
