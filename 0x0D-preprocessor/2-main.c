41ad12a6601acd00fa4845aae5b13377f5b1b070
Super User <rahmakh248@gmail.com>
2023-09-11 11:15:46 -0700

    done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+/*
+ * file : 2-main.c
+ * code written by : Rahma Khaled
+ */
+#include <stdio.h>
+/**
+  * main - prints the name of the file it was compiled from
+  * @void: accepts nothing
+  * Return: 0 on success
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
