58ecb7aa8eb40c1aa20fc9dabde1f2f2e759f728
Super User <rahmakh248@gmail.com>
2023-09-11 08:25:42 -0700

    done

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+/*
+ * file : 2-print_dog.c
+ * code written by : Rahma Khaled
+ */
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_dog -  prints a struct dog
+ * @d : pointer of type dog
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		d->name ? (printf("Name: %s\n", d->name)) : (printf("Name: (nil\n)"));
+		printf("Age: %f\n", d->age);
+		d->owner ? (printf("Owner: %s\n", d->owner)) : (printf("Owner: (nil\n)"));
+	}
+}
