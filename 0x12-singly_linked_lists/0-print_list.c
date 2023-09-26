5cffd4691525af662e7a2792b7adb7aefdf1792e
rahma khaled <rahmakh248@gmail.com>
2023-09-26 11:35:00 -0700

    upload

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,11 +15,9 @@ size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
-	while (h != NULL)
+	while (h)
 	{
-		printf("%s", h->str);
+		printf("[%d] %s\n",h->len, h->str);
 		h = h->next;
 		count++;
 	}
