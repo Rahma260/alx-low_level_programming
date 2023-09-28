3e06d57c44f8be4eadf308d3913582cc77d62baf
rahma khaled <rahmakh248@gmail.com>
2023-09-28 07:58:09 -0700

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,19 +11,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i;
-	unsigned int newb;
+	unsigned int newb = 0;
 
 	if (b == NULL)
 		return (0);
-	for (i = 0, newb = 0; b[i] != '\0'; i++)
+	while (*b != '\0')
 	{
-		if (b[i] != '0' || b[i] != '1')
+		newb <<= 1;
+		if (*b != '1' && *b != '0')
 			return (0);
-		else if (b[i] == '1')
-			newb = (newb << 1) | 1;
-		else if (b[i] == '0')
-			newb = newb << 1;
+		else if (*b == '1')
+			newb |= 1;
+		b++;
 	}
 	return (newb);
 }
