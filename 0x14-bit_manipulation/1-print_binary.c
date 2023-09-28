ead8f24302aeffcab09db960e3c43e6d225145c6
rahma khaled <rahmakh248@gmail.com>
2023-09-28 12:53:54 -0700

    done

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,6 +3,7 @@
  * code written by : Rahma Khaled
  */
 #include "main.h"
+#include <stdio.h>
 /**
  * print_binary -  prints the binary representation of a number.
  * @n : the number to be converted
@@ -12,14 +13,14 @@ char *print_binary(unsigned long int n)
 {
 	if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return;
 	}
 	else if (n == 1)
 	{
-		_putchar ('1');
+		putchar ('1');
 		return;
 	}
 	print_binary(n >> 1);
-	_putchar('0' + (n & 1));
+	putchar('0' + (n & 1));
 }
